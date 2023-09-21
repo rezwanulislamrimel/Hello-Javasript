2872d6a65242c1f6a92dd4487ec93390f2f392c8
Rezwanulrimele <rezwanulrimel@gmail.comm>
2023-09-21 22:00:15 +0600

    add parameter section

---
+++ function.js
@@ -0,0 +1,7 @@
+function BringJs() {
+    console.log("At First lern html");
+    console.log("then lern css");
+    console.log("thne learn framwork");
+    console.log("like bootstrap, tailwind, and daisyUI");
+}
+BringJs();
---
+++ parameter.js
@@ -0,0 +1,26 @@
+function BringSingara   (many) {
+    console.log("tk deo");
+    console.log("singara neo");
+ 
+    console.log(many);
+}
+
+BringSingara(100);
+
+
+
+function add(number1, number2) {
+    console.log(number1, number2);
+    var add = number1 + number2;
+    console.log(add)
+}
+
+add(1, 3);
+
+
+function TotalAdd(a, b, c, d, e) {
+    console.log(a, b, c, d, e);
+    var add = a + b + c + d + e;
+    console.log(add);
+}
+TotalAdd(1, 2, 3, 4, 5);
